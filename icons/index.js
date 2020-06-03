101eb4894a3f5711f381ef8512c89082665b5e24
jayrald07 <geraldempino@gmailc.com>
2020-06-03 22:05:24 +0800

    Pushing my react app

---
+++ icons/index.js
@@ -0,0 +1,9 @@
+import facebook from './facebook.svg';
+import twitter from './twitter.svg';
+import envelope from './envelope.svg';
+
+export {
+    facebook,
+    twitter,
+    envelope
+};
---
+++ precache-manifest.35c8da20a9f03e0b3b9b39b9ca63bb5d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e27cf304bc5de5f0fa5b80c4cb6948",
+    "url": "/index.html"
+  },
+  {
+    "revision": "605e7a5fdc43b2e7983a",
+    "url": "/static/css/2.3a52a6be.chunk.css"
+  },
+  {
+    "revision": "eca0ca00ee39c7ecf996",
+    "url": "/static/css/main.3d032717.chunk.css"
+  },
+  {
+    "revision": "605e7a5fdc43b2e7983a",
+    "url": "/static/js/2.bb393e75.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bb393e75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eca0ca00ee39c7ecf996",
+    "url": "/static/js/main.ae7353d7.chunk.js"
+  },
+  {
+    "revision": "f2dbb0ff9df637cd6da1",
+    "url": "/static/js/runtime-main.18dfab9d.js"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "d0c98f456dfb53bb1d45fbdf41149af7",
+    "url": "/static/media/origami.d0c98f45.ttf"
+  }
+]);
